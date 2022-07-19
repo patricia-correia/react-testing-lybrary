e5c9577bc27f272bf0c2f59ae6c8828de92c6073
patricia-correia <pncorreia25@gmail.com>
2022-07-19 18:25:23 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('', () => {
+  test('o titulo deve está na tela em um h2', () => {
+    render(<NotFound />);
+    expect(screen
+      .getByRole('heading', { name: /Page requested not found/i, level: 2 }))
+      .toBeInTheDocument();
+  });
+  test(' testa se há uma imagem renderizada na tela', () => {
+    render(<NotFound />);
+    const imagem = screen
+      .getByAltText(/Pikachu crying because the page requested was not found/i);
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(imagem).toHaveAttribute('src', url);
+  });
+});
