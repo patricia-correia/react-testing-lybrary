ffe2ec20fc885db458144e9df71a3e833c1fb9e4
patricia-correia <pncorreia25@gmail.com>
2022-07-19 18:01:29 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,5 +1,27 @@
-describe('testando a pagina sobre da pokedex', () => {
-  test('', () => {
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import About from '../pages/About';
 
+describe('testando o conteúdo da página About', () => {
+  test('o titulo deve está na tela em um h2', () => {
+    render(<About />);
+    expect(screen.getByRole('heading', { name: /About Pokédex/i, level: 2 }))
+      .toBeInTheDocument();
+  });
+  test('testa a quantidade de paragrafos que é renderizado na pagina', () => {
+    render(<About />);
+    //  const paragrafo = (screen.getAllByRole('paragraph'));
+    //  expect(paragrafo).toHaveLength(2);
+
+    const part1 = (/This application ... /i);
+    const part2 = (/One can filter Pokémons by type.../i);
+    const textAbout = screen.getByText(part1 && part2);
+    expect(textAbout).toBeInTheDocument();
+  });
+  test(' testa se há uma imagem renderizada na tela', () => {
+    render(<About />);
+    const imagem = screen.getByAltText(/pokédex/i);
+    const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(imagem).toHaveAttribute('src', url);
   });
 });
